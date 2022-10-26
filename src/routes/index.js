51605ab33cf985089e13092c2b4058e01dc281ed
Ernes <90705607+Erne5@users.noreply.github.com>
2022-10-26 18:30:43 -0300

    Post Admin completo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const userMiddleware = require('./Middlewares/userMiddleware');
+const adminMiddleware = require('./Middlewares/adminMiddleware');
 const router = Router();
 
 
-router.use('/user', userMiddleware)
+router.use('/user', userMiddleware);
+router.use('/admin', adminMiddleware);
 
 
 
