76c1f56eed7237b3e8a540429182b50805a0157e
Ernes <90705607+Erne5@users.noreply.github.com>
2022-10-26 18:43:54 -0300

    agregados description e image

--- src/routes/Controllers/postUser.js
+++ src/routes/Controllers/postUser.js
@@ -2,8 +2,8 @@ const {Professional} = require('../../models/professional.js')
 
 const postUser = async(req, res) => {
     try {
-        const {fullName, phoneNumber, email, occupation, address} = req.body;
-        if (!fullName || !phoneNumber || !email || !occupation || !address){
+        const {fullName, phoneNumber, email, occupation, address, description, image} = req.body;
+        if (!fullName || !phoneNumber || !email || !occupation || !address || !description){
             return res.status(400).send('missing value detected.')
         }
         else {
@@ -12,7 +12,9 @@ const postUser = async(req, res) => {
                 phoneNumber,
                 email,
                 occupation,
-                address
+                address,
+                description,
+                image
             })
             return res.status(201).send('new Professional created.')
         }
