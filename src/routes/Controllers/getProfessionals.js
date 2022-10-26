040cb3897358f5591e10c3432abad32d9d9b8486
IggyMartin <102999057+IggyMartin@users.noreply.github.com>
2022-10-26 20:33:59 -0300

    get professionals

--- src/routes/Controllers/getProfessionals.js
+++ src/routes/Controllers/getProfessionals.js
@@ -1,9 +1,9 @@
-const {Professional} = require('../../models/professional.js')
+const {Professional} = require('../../db')
 
 const getProfessionals = async(req, res) => {
     try {
         const allProfessionals = await Professional.findAll()
-        return res.send(allProfessionals)
+        return res.json(allProfessionals)
     } catch (error) {
         return res.status(404).send(console.log(error))
     }
