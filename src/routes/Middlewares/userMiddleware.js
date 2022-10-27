cff5b22019f45de2b6335e2286af06a5a92395ab
IggyMartin <102999057+IggyMartin@users.noreply.github.com>
2022-10-27 17:23:35 -0300

    Ruta get de clients con actualizacion del MiddleWare

--- src/routes/Middlewares/userMiddleware.js
+++ src/routes/Middlewares/userMiddleware.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
+const {getClients} = require('../Controllers/getClients.js')
 const {getProfessionals} = require('../Controllers/getProfessionals.js');
 const {postUser} = require('../Controllers/postUser.js');
 const router = Router();
 
+router.get('', getClients)
 router.get('', getProfessionals)
 router.post('', postUser);
 
