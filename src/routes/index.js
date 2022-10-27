3a0067e1ec131e3d9af005d9483c2f517c695a16
Federico Montanari <fedemontanari@outlook.com>
2022-10-27 18:41:36 -0300

    GET y POST client

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const professionalMiddleware = require("./Middlewares/professionalMiddleware");
 const adminMiddleware = require("./Middlewares/adminMiddleware");
 const occupationMiddleware = require("./Middlewares/occupationMiddleware");
+const clientMiddleware = require("./Middlewares/clientMiddleware");
 const router = Router();
 
 router.use("/professional", professionalMiddleware);
 router.use("/admin", adminMiddleware);
 router.use("/occupation", occupationMiddleware);
+router.use("/client", clientMiddleware);
 
 module.exports = router;
