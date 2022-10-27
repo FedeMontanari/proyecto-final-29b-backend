5b3b70681f38981073000f9b3a5e6090b5fc9623
Federico Montanari <fedemontanari@outlook.com>
2022-10-27 17:49:33 -0300

    GET admin - Formato con PRETTIER

--- src/routes/Middlewares/adminMiddleware.js
+++ src/routes/Middlewares/adminMiddleware.js
@@ -1,7 +1,9 @@
-const {Router} = require('express');
-const {postAdmin} = require('../Controllers/postAdmin.js');
+const { Router } = require("express");
+const { postAdmin } = require("../Controllers/postAdmin");
+const { getAdmin } = require("../Controllers/getAdmin");
 const router = Router();
 
-router.post('', postAdmin);
+router.post("", postAdmin);
+router.get("", getAdmin);
 
 module.exports = router;
