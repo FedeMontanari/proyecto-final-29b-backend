950feacf3ebe67f833d76f59d2d49b287d4859fe
FedeMontanari <95886622+FedeMontanari@users.noreply.github.com>
2022-10-28 14:14:42 -0300

    Revert "Ready for deplyment"

--- src/routes/Middlewares/clientMiddleware.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getClient } = require("../Controllers/getClient.js");
-const { postClient } = require("../Controllers/postClient.js");
-const router = Router();
-
-router.get("", getClient);
-router.post("", postClient);
-
-module.exports = router;
--- src/routes/Middlewares/occupationMiddleware.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { postOccupation } = require("../Controllers/postOccupation");
-const { getOccupation } = require("../Controllers/getOccupation");
-const router = Router();
-
-router.post("", postOccupation);
-router.get("", getOccupation);
-
-module.exports = router;
