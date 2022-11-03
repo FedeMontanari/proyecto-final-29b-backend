9ed9be3054ee06a5d4300f90391009ed131b3b8c
Ernes <90705607+Erne5@users.noreply.github.com>
2022-11-03 06:24:28 -0300

    ruta PUT/DELETE Admin Completa

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { Admin } = require("../db");
+const admin = require("../models/admin");
 const router = Router();
 
 router.post("/", async (req, res) => {
@@ -28,5 +29,57 @@ router.get("/", async (req, res) => {
     return res.status(404).send(console.log(error));
   }
 });
+
+router.put('/id/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+      return res.status(400).send("missing value detected.");
+    }
+    if (id) {
+      const adminFound = await Admin.findOne({
+        where: {
+          id: id
+        }
+      })
+      if (adminFound) {
+        const updateAdmin = {
+          fullName,
+          email,
+          password
+        }
+        adminFound.update(updateAdmin)
+        return res.status(200).send('Admin Updated successfully.')
+      }
+    }
+  } catch (e) {
+    return res.status(400).send(console.log(e))
+  }
+})
+
+router.delete('/id/:id', async (req, res) => {
+  try {
+      const {id} = req.params;
+      if(!id) res.status(400).send('Missing value detected.')
+      else {
+          let adminFound = await Admin.findOne({
+              where: {
+                  id: id
+              }
+          });
+          if (adminFound.length !==0) {
+              Admin.destroy({
+                  where: {
+                      id: id
+                  }
+              })
+              return res.status(200).send('Admin deleted.')
+          } else res.status(404).send('Admin not found.')
+      }
+  } catch (e) {
+      res.status(400).send(console.log(e))
+  }
+})
 
 module.exports = router;
