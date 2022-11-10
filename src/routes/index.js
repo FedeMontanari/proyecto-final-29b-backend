a74e5abb882f4912cb55d65e44d365f3aa0726d6
Ernes <90705607+Erne5@users.noreply.github.com>
2022-11-10 12:53:19 -0300

    index ruta review agregada

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ const adminMiddleware = require("./admin");
 const occupationMiddleware = require("./occupation");
 const userMiddleware = require("./user");
 const specializationMiddleware = require("./specialization");
+const reviewMiddleware = require("./review");
 const router = Router();
 
 router.use("/admin", adminMiddleware);
 router.use("/occupation", occupationMiddleware);
 router.use("/user", userMiddleware);
 router.use("/specialization", specializationMiddleware);
-
+router.use("/review", reviewMiddleware);
 
 module.exports = router;
